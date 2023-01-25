748bdefd2c67d897a27a86e365da48af40adbcd1
Alexander Semenov <asemenov.java@gmail.com>
2023-01-25 19:17:30 +0300

    write the conditions for finding the roots

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var a float64
@@ -19,4 +22,21 @@ func main() {
 	fmt.Scan(&c)
 
 	D := (b * b) - 4*a*c
+
+	if D > 0 {
+		var x1 float64
+		var x2 float64
+
+		x1 = (-b + math.Sqrt(D)) / (2 * a)
+		x2 = (-b - math.Sqrt(D)) / (2 * a)
+
+	} else if D == 0 {
+
+		var x float64
+
+		x = (-b) / (2 * a)
+	} else {
+
+	}
+
 }
